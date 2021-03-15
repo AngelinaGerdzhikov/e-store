f099ac86f7b5f90230a467def6170172313b0e32
Angelina Gerdzhikov <angelina.gerdzhikov@gmail.com>
2021-03-15 15:37:41 +0200

    Added recipe details

--- src/app/recipes/components/recipe-details/recipe-details.component.ts
+++ src/app/recipes/components/recipe-details/recipe-details.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { take } from 'rxjs/operators';
+import { Recipe } from 'shared/models/recipe';
+import { RecipesService } from 'shared/services/recipes/recipes.service';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
   styleUrls: ['./recipe-details.component.scss']
 })
-export class RecipeDetailsComponent implements OnInit {
+export class RecipeDetailsComponent {
+  id: string;
+  recipe: Recipe;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private recipeService: RecipesService
+  ) {
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      this.recipeService.get(this.id)
+        .pipe(take(1))
+        .subscribe(recipe => this.recipe = recipe);
+    }
   }
-
 }
--- src/app/shared/services/recipes/recipes.service.ts
+++ src/app/shared/services/recipes/recipes.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Recipe } from 'shared/models/recipe';
 
@@ -11,6 +12,10 @@ export class RecipesService {
 
   constructor(private db: AngularFireDatabase) { }
 
+  get(recipeId: string) {
+    return this.db.object(`/recipes/${recipeId}`).valueChanges() as Observable<Recipe>;
+  }
+
   getAll() {
     return this.db.list(`/recipes`).snapshotChanges().pipe(
       map(c => c.map(c => ({ key: c.payload.key, ...c.payload.val() as any })))
@@ -20,4 +25,13 @@ export class RecipesService {
   create(recipe: Recipe) {
     return this.db.list(`/recipes`).push(recipe);
   }
+
+  update(recipeId, recipe) {
+    return this.db.object(`recipes/${recipeId}`).update(recipe);
+  }
+
+  delete(recipeId) {
+    return this.db.object(`recipes/${recipeId}`).remove();
+  }
+
 }
