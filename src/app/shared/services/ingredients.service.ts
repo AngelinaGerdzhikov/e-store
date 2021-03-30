2d96350dd7acbac732a5f145549fd0cb395f9d6c
Angelina Gerdzhikov <angelina.gerdzhikov@gmail.com>
2021-03-30 11:47:31 +0300

    Deleted shopping-list components

--- src/app/shared/services/ingredients.service.ts
+++ src/app/shared/services/ingredients.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { Ingredient } from 'shared/models/ingredient';
+import { Product } from 'shared/models/product';
 
 import { DataService } from './data/data.service';
 import { ProductService } from './product/product.service';
@@ -23,7 +25,13 @@ export class IngredientsService extends DataService<Ingredient> {
   addToShoppingCart(ingredient: Ingredient) {
     return this.productService.get(ingredient.productUid).pipe(
       switchMap(product => {
-        if (product) return this.shoppingCartService.addToCart(product, ingredient.quantity);
+        if (product) {
+          return this.shoppingCartService.addToCart(
+            new Product({...product, key: ingredient.productUid }),
+            ingredient.quantity
+          );
+        }
+        return of(null);
       })
     );
   }
